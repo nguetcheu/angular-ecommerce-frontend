49070e0a0970e52ee23744c7ec09976672072cd9
nguetcheu <dnguetcheu@gmail.com>
2022-12-04 19:30:52 +0100

    ajout de la méthiode getProductList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
